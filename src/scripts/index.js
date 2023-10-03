db12c1408caebd599da4f4b95ff58b8799b21ab1
Wastend <wastend318@gmail.com>
2023-10-03 23:41:23 +0600

    add menu and start page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,29 @@
-const botton__business = document.getElementById('botton__business')
-const botton__customers = document.getElementById('botton__customers')
-const header__buttons = [botton__business, botton__customers]
+const button__business = document.getElementById('button__business')
+const button__customers = document.getElementById('button__customers')
+const header__buttons = [button__business, button__customers]
+
+const header__bands = document.getElementById('header__bands')
+const menu__button_close = document.getElementById('menu__button_close')
+const menu = document.getElementById('menu')
+const menu__buttons = [header__bands, menu__button_close]
+
 
 header__buttons.forEach(element => {
     element.addEventListener('click', (e) => {
         e.preventDefault()
         if (!element.classList.contains('active')) {
             element.classList.add('active')
-            element.getAttribute('id') === 'botton__business' ? botton__customers.classList.remove('active') : botton__business.classList.remove('active')
+            element.getAttribute('id') === 'button__business'
+                ? button__customers.classList.remove('active')
+                : button__business.classList.remove('active')
         }
+    })
+})
+
+menu__buttons.forEach(element => {
+    element.addEventListener('click', (e) => {
+        e.preventDefault()
+        menu.classList.contains('active') ? menu.classList.remove('active') : menu.classList.add('active')
+
     })
 })
