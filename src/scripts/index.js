b197e06c7b694e40d66ecdb8f35570a4c4694046
Wastend <wastend318@gmail.com>
2023-10-12 14:44:06 +0600

    menu ready

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,16 @@
-const button__business = document.getElementById('button__business')
-const button__customers = document.getElementById('button__customers')
+const button__business = document.getElementsByName('button__business')
+const button__customers = document.getElementsByName('button__customers')
 const header__buttons = [button__business, button__customers]
+
+const header__button_contact = document.getElementById('header__button_contact')
+const header__buttons_apps = document.getElementById('header__buttons_apps')
 
 const header__bands = document.getElementById('header__bands')
 const menu__button_close = document.getElementById('menu__button_close')
 const menu = document.getElementById('menu')
 const menu__buttons = [header__bands, menu__button_close]
+const menu__button_contact = document.getElementById('menu__button_contact')
+const menu__buttons_apps = document.getElementById('menu__buttons_apps')
 
 const buttons__contact = document.getElementsByName('button__contact')
 const form = document.getElementById('window')
@@ -36,22 +41,48 @@ const cookie = document.getElementById('cookie')
 const cookie__buttons = document.getElementsByName('cookie__button')
 
 header__buttons.forEach(element => {
-  element.addEventListener('click', (e) => {
-    e.preventDefault()
-    if (!element.classList.contains('active')) {
-      element.classList.add('active')
-      element.getAttribute('id') == 'button__business'
-        ? button__customers.classList.remove('active')
-        : button__business.classList.remove('active')
-    }
-  })
+  element.forEach(el => {
+    el.addEventListener('click', (e) => {
+      e.preventDefault()
+      if (el.getAttribute('name') == 'button__business') {
+        !button__business[0].classList.contains('active') && button__business[0].classList.add('active')
+        button__customers[0].classList.remove('active')
+        if (window.innerWidth > 375) {
+          header__button_contact.classList.remove('invisible')
+          header__buttons_apps.classList.add('invisible')
+        }
+        else {
+          menu__button_contact.classList.remove('invisible')
+          menu__buttons_apps.classList.add('invisible')
+        }
+      }
+      else {
+        !button__customers[0].classList.contains('active') && button__customers[0].classList.add('active')
+        button__business[0].classList.remove('active')
+        if (window.innerWidth > 375) {
+          header__button_contact.classList.add('invisible')
+          header__buttons_apps.classList.remove('invisible')
+        }
+        else {
+          menu__button_contact.classList.add('invisible')
+          menu__buttons_apps.classList.remove('invisible')
+        }
+      }
+    })
+  }
+  )
 })
 
 menu__buttons.forEach(element => {
   element.addEventListener('click', (e) => {
     e.preventDefault()
-    menu.classList.contains('active') ? menu.classList.remove('active') : menu.classList.add('active')
-
+    if (menu.classList.contains('active')) {
+      menu.classList.remove('active')
+      header__bottom.style.display = 'flex'
+    } else {
+      menu.classList.add('active')
+      header__bottom.style.display = 'none'
+    }
   })
 })
 
